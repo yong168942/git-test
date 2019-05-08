c6d12384ec33e47ea161719c43082d18c3231626
jcyong <ryo168942@gmail.com>
2019-05-08 21:34:39 +0900

    add sum minus

--- Calculator.java
+++ Calculator.java
@@ -3,7 +3,14 @@ package com.git.sroucetree;
 public class Calculator {
 
   public static void main(String[] agrs) {
-      
+
   }
 
+  private static int sum(int a, int b) {
+    return a + b;
+  }
+
+  pulbic static int minus(int a,int b) {
+    return a - b;
+  }
 }
